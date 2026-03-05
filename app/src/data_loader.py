ee67c1b56f648c45c80510817c7ccee955648514
KevinOFL <kevinsilvaofl@gmail.com>
2026-03-05 17:33:42 +0000

    feat(Modulo-de-carregamento-dos-dados): Modulo que serve para o carregamento dos dados tanto para treinamento de modelos quanto para tratar novos dados enviados para predicao de modelos ja construidos
    BREAKING CHANGE:

--- app/src/data_loader.py
+++ app/src/data_loader.py
@@ -1,6 +1,8 @@
 import pandas as pd
-from app.src.pre_processing import pre_processing as pp
+
 from app.config.log_config import logger_data_processing
+from app.src.pre_processing import pre_processing as pp
+
 
 def get_clean_data(path="app/data/base_de_dados_pede_2024.xlsx"):
     df_2022 = pd.read_excel(path, sheet_name="PEDE2022")
@@ -11,131 +13,127 @@ def get_clean_data(path="app/data/base_de_dados_pede_2024.xlsx"):
     df_filtered_2023 = pp.padronize_collumns(df_2023)
     df_filtered_2024 = pp.padronize_collumns(df_2024)
 
-    df_filtered_2022 = pp.create_target_variable(
-        df_filtered_2022, df_filtered_2023
-    )
-    df_filtered_2023 = pp.create_target_variable(
-        df_filtered_2023, df_filtered_2024
-    )
+    df_filtered_2022 = pp.create_target_variable(df_filtered_2022, df_filtered_2023)
+    df_filtered_2023 = pp.create_target_variable(df_filtered_2023, df_filtered_2024)
 
     df_filtered_2022 = pp.exclude_columns(
         df_filtered_2022,
         [
-            'ra',
-            'fase',
-            'turma',
-            'nome',
-            'ano_nasc',
-            'instituicao_de_ensino',
-            'pedra_20',
-            'pedra_21',
-            'pedra_22',
-            'cg',
-            'cf',
-            'ct',
-            'no_av',
-            'avaliador1',
-            'rec_av1',
-            'avaliador2',
-            'rec_av2',
-            'avaliador3',
-            'rec_av3',
-            'avaliador4',
-            'rec_av4',
-            'rec_psicologia',
-            'indicado',
-            'atingiu_pv',
-            'fase_ideal',
-            'destaque_ieg',
-            'destaque_ida',
-            'destaque_ipv',
-            'defas'
-        ]
+            "ra",
+            "fase",
+            "turma",
+            "nome",
+            "ano_nasc",
+            "instituicao_de_ensino",
+            "pedra_20",
+            "pedra_21",
+            "pedra_22",
+            "cg",
+            "cf",
+            "ct",
+            "no_av",
+            "avaliador1",
+            "rec_av1",
+            "avaliador2",
+            "rec_av2",
+            "avaliador3",
+            "rec_av3",
+            "avaliador4",
+            "rec_av4",
+            "rec_psicologia",
+            "indicado",
+            "atingiu_pv",
+            "fase_ideal",
+            "destaque_ieg",
+            "destaque_ida",
+            "destaque_ipv",
+            "defas",
+        ],
     )
 
     df_filtered_2023 = pp.correction_collum_age(df_filtered_2023, 2023)
     df_filtered_2023 = pp.exclude_columns(
         df_filtered_2023,
         [
-            'ra',
-            'fase',
-            'data_de_nasc',
-            'pedra_2023',
-            'turma',
-            'nome_anonimizado',
-            'instituicao_de_ensino',
-            'pedra_20',
-            'pedra_21',
-            'pedra_22',
-            'pedra_23',
-            'inde_22',
-            'inde_23',
-            'cg',
-            'cf',
-            'ct',
-            'no_av',
-            'avaliador1',
-            'rec_av1',
-            'avaliador2',
-            'rec_av2',
-            'avaliador3',
-            'rec_av3',
-            'avaliador4',
-            'rec_av4',
-            'rec_psicologia',
-            'indicado',
-            'atingiu_pv',
-            'fase_ideal',
-            'destaque_ieg',
-            'destaque_ida',
-            'destaque_ipv',
-            'destaque_ipv.1',
-            'defasagem',
-            'ipp'
-        ]
+            "ra",
+            "fase",
+            "data_de_nasc",
+            "pedra_2023",
+            "turma",
+            "nome_anonimizado",
+            "instituicao_de_ensino",
+            "pedra_20",
+            "pedra_21",
+            "pedra_22",
+            "pedra_23",
+            "inde_22",
+            "inde_23",
+            "cg",
+            "cf",
+            "ct",
+            "no_av",
+            "avaliador1",
+            "rec_av1",
+            "avaliador2",
+            "rec_av2",
+            "avaliador3",
+            "rec_av3",
+            "avaliador4",
+            "rec_av4",
+            "rec_psicologia",
+            "indicado",
+            "atingiu_pv",
+            "fase_ideal",
+            "destaque_ieg",
+            "destaque_ida",
+            "destaque_ipv",
+            "destaque_ipv.1",
+            "defasagem",
+            "ipp",
+        ],
     )
 
     df_filtered_2024 = pp.exclude_columns(
         df_filtered_2024,
         [
-            'ra',
-            'fase',
-            'pedra_2024',
-            'turma',
-            'data_de_nasc',
-            'nome_anonimizado',
-            'instituicao_de_ensino',
-            'pedra_20',
-            'pedra_21',
-            'pedra_22',
-            'pedra_23',
-            'cg',
-            'cf',
-            'ct',
-            'inde_22',
-            'inde_23',
-            'no_av',
-            'avaliador1',
-            'rec_av1',
-            'avaliador2',
-            'rec_av2',
-            'avaliador3',
-            'avaliador4',
-            'avaliador5',
-            'avaliador6',
-            'rec_psicologia',
-            'indicado',
-            'atingiu_pv',
-            'fase_ideal',
-            'destaque_ieg',
-            'destaque_ida',
-            'destaque_ipv',
-            'escola',
-            'ativo/_inativo',
-            'ativo/_inativo.1',
-            'defasagem',
-            'ipp'
-        ]
+            "ra",
+            "fase",
+            "pedra_2024",
+            "turma",
+            "data_de_nasc",
+            "nome_anonimizado",
+            "instituicao_de_ensino",
+            "pedra_20",
+            "pedra_21",
+            "pedra_22",
+            "pedra_23",
+            "cg",
+            "cf",
+            "ct",
+            "inde_22",
+            "inde_23",
+            "no_av",
+            "avaliador1",
+            "rec_av1",
+            "avaliador2",
+            "rec_av2",
+            "avaliador3",
+            "avaliador4",
+            "avaliador5",
+            "avaliador6",
+            "rec_psicologia",
+            "indicado",
+            "atingiu_pv",
+            "fase_ideal",
+            "destaque_ieg",
+            "destaque_ida",
+            "destaque_ipv",
+            "escola",
+            "ativo/_inativo",
+            "ativo/_inativo.1",
+            "defasagem",
+            "ipp",
+        ],
     )
 
     df_filtered_2022 = pp.padronize_names_for_collumns(df_filtered_2022)
@@ -155,8 +153,72 @@ def get_clean_data(path="app/data/base_de_dados_pede_2024.xlsx"):
 
     df_filtered_2024 = pp.correction_collum_inde(df_filtered_2024)
     df_filtered_2024 = pp.aplication_median_for_nan(df_filtered_2024, "ing")
-    
+
+    df_filtered_2022 = pp.create_columns_for_discrepancies_in_subjects(df_filtered_2022)
+    df_filtered_2023 = pp.create_columns_for_discrepancies_in_subjects(df_filtered_2023)
+    df_filtered_2024 = pp.create_columns_for_discrepancies_in_subjects(df_filtered_2024)
+
+    df_filtered_2022 = pp.create_column_for_discrepancie_in_ieg_inde(df_filtered_2022)
+    df_filtered_2023 = pp.create_column_for_discrepancie_in_ieg_inde(df_filtered_2023)
+    df_filtered_2024 = pp.create_column_for_discrepancie_in_ieg_inde(df_filtered_2024)
+
     return df_filtered_2022, df_filtered_2023, df_filtered_2024
+
+
+def loading_data_prediction(archive):
+    df = pp.padronize_collumns(archive)
+    df = pp.exclude_columns(
+        df,
+        [
+            "ra",
+            "fase",
+            "pedra_2024",
+            "turma",
+            "data_de_nasc",
+            "nome_anonimizado",
+            "instituicao_de_ensino",
+            "pedra_20",
+            "pedra_21",
+            "pedra_22",
+            "pedra_23",
+            "cg",
+            "cf",
+            "ct",
+            "inde_22",
+            "no_av",
+            "avaliador1",
+            "rec_av1",
+            "avaliador2",
+            "rec_av2",
+            "avaliador3",
+            "avaliador4",
+            "avaliador5",
+            "avaliador6",
+            "rec_psicologia",
+            "indicado",
+            "atingiu_pv",
+            "fase_ideal",
+            "destaque_ieg",
+            "destaque_ida",
+            "destaque_ipv",
+            "escola",
+            "ativo/_inativo",
+            "ativo/_inativo.1",
+            "defasagem",
+            "ipp",
+        ],
+    )
+    df = pp.padronize_names_for_collumns(df)
+    df = pp.padronize_column_gender(df)
+    df = pp.studied_english(df)
+    df = pp.aplication_median_for_nan(df, "ing")
+    df = pp.correction_collum_inde(df)
+    df = pp.aplication_median_for_nan(df, "ing")
+    df = pp.create_columns_for_discrepancies_in_subjects(df)
+    df = pp.create_column_for_discrepancie_in_ieg_inde(df)
+
+    return df
+
 
 if __name__ == "__main__":
     # Se você rodar python data_loader.py no terminal, ele cai aqui para testar
